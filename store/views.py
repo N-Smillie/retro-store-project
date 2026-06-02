8503b3b0ad9fbe22684db8829b75cf3a5467e13f
N-Smillie <dominategamehs@gmail.com>
2026-06-02 10:50:49 +0100

    Build store page with game cards and image display

--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Game
 
-# Create your views here.
+
+def all_products(request):
+    """
+    Display all games
+    """
+
+    games = Game.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'store/store.html', context)
