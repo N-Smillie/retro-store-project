8f89376cedf2de1c2201ed2247f4ab1a63fcfbc2
N-Smillie <dominategamehs@gmail.com>
2026-06-05 09:55:55 +0100

    Added placeholders to Order form

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,7 +22,28 @@ class OrderForm(forms.ModelForm):
 
         super().__init__(*args, **kwargs)
 
+        self.fields['full_name'].widget.attrs[
+            'autofocus'
+        ] = True
+
+        placeholders = {
+            'full_name': 'Enter your full name',
+            'email': 'Enter your email address',
+            'phone_number': 'Enter your phone number',
+            'street_address1': 'House number and street',
+            'street_address2': '(optional)',
+            'town_or_city': 'Town or city',
+            'postcode': 'Postcode',
+            'county': 'County (optional)',
+            'country': 'Country',
+        }
+
         for field in self.fields:
             self.fields[field].widget.attrs[
                 'class'
             ] = 'form-control'
+
+            if field in placeholders:
+                self.fields[field].widget.attrs[
+                    'placeholder'
+                ] = placeholders[field]
